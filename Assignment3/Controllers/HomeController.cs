067a70455309ffcac81d655de9e0a5cabf0851b8
Dallin Jared <66185492+djared85@users.noreply.github.com>
2021-02-01 17:19:52 -0700

    Assignment 3 Commit

---
+++ Assignment3/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Assignment3.Models;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() => View();
+
+        public IActionResult Privacy() => View();
+
+        [HttpGet]
+        public IActionResult AddMovie() => View();
+
+        [HttpPost]
+        public IActionResult AddMovie(MovieForm movieResponse)
+        {
+            TempStorage.AddMovie(movieResponse);
+            return View("Confirmation", movieResponse);
+        }
+
+        [HttpGet]
+        public IActionResult MovieList() => View(TempStorage.MovieForms);
+
+        [HttpGet]
+        public IActionResult Confirmation() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Assignment3/Models/TempStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment3.Models
+{
+    public static class TempStorage
+    {
+        private static List<MovieForm> movies = new List<MovieForm>();
+
+        public static IEnumerable<MovieForm> MovieForms => movies;
+
+        public static void AddMovie(MovieForm movie)
+        {
+            movies.Add(movie);
+        }
+    }
+}
